4003359c38523464e51707335d6bed34d6f4f401
Srikanth Maturu <srikanthmaturu@users.noreply.github.com>
2017-11-25 14:49:13 -0600

    Add nohup

--- setup.sh
+++ setup.sh
@@ -13,4 +13,4 @@ cd ../../
 unzip fluxSimExpressed.fa.faa.zip
 unzip SPAdes_transcripts_merged.fasta.unique.zip
 cd SequencesClusteringTool/build/
-./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 2 ../../fluxSimExpressed.fa.faa.unique fasta ../../SPAdes_transcripts_merged.fasta.unique.faa fasta 70 1 > spades_edlib_results.txt & 
+nohup ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 2 ../../fluxSimExpressed.fa.faa.unique fasta ../../SPAdes_transcripts_merged.fasta.unique.faa fasta 70 1 > spades_edlib_results.txt & 
