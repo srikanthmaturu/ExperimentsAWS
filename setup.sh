cfb663e917e714f0909c266bfc0faea4f5a5fe22
Srikanth Maturu <srikanthmaturu@users.noreply.github.com>
2017-11-25 13:03:48 -0600

    Add make command

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ cd SequencesClusteringTool-master/
 mkdir build
 cd build
 cmake ..
+make seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat
 #Experiment 1
 ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 2 ../../fluxSimExpressed.fa.faa.unique fasta ../../SPAdes_transcripts_merged.fasta.unique.faa fasta 70 1 > spades_edlib_results.txt & 
