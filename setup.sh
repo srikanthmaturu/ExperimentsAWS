26ce46807441f556d58f5c3872a414dc1e3390c2
Srikanth Maturu <srikanthmaturu@outlook.com>
2017-11-25 23:50:23 -0600

    Add cufflink,stringtie data and experiment commands

--- setup.sh
+++ setup.sh
@@ -8,9 +8,28 @@ mkdir build
 cd build
 cmake ..
 make seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat
-#Experiment 1
 cd ../../
 unzip fluxSimExpressed.fa.faa.zip
 unzip SPAdes_transcripts_merged.fasta.unique.zip
+unzip cufflinks_transcripts.fasta.unique.faa.zip
+unzip stringTie_transcripts.fasta.unique.faa.zip
+
+#Experiment 1
 cd SequencesClusteringTool/build/
 nohup ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 2 ../../fluxSimExpressed.fa.faa.unique fasta ../../SPAdes_transcripts_merged.fasta.unique.faa fasta 70 1 > spades_edlib_results.txt & 
+
+#Experiment 2
+cd SequencesClusteringTool/build/
+nohup ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 1 ../../fluxSimExpressed.fa.faa.unique fasta ../../cufflinks_transcripts.fasta.unique.faa fasta 0 1 70 1 > cufflinks_falconn_results.txt & 
+
+#Experiment 3
+cd SequencesClusteringTool/build/
+nohup ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 2 ../../fluxSimExpressed.fa.faa.unique fasta ../../cufflinks_transcripts.fasta.unique.faa fasta 70 1 > cufflinks_edlib_results.txt & 
+
+#Experiment 4
+cd SequencesClusteringTool/build/
+nohup ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 1 ../../fluxSimExpressed.fa.faa.unique fasta ../../stringTie_transcripts.fasta.unique.faa fasta 0 1 70 1 > stringtie_falconn_results.txt & 
+
+#Experiment 5
+cd SequencesClusteringTool/build/
+nohup ./seq_anlyzer_DT_1_NL_3_LT_2_NHT_32_NHB_14_NP_300_TH_70_PT_SparseVectorFloat 2 ../../fluxSimExpressed.fa.faa.unique fasta ../../stringTie_transcripts.fasta.unique.faa fasta 70 1 > stringtie_edlib_results.txt & 
